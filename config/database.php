a92af855e1605de7a70cdbcd089d36e08b3c723e
meetkauputiyaa <meetkapuriya4071@gmail.com>
2026-07-01 14:11:58 +0530

    Update pgsql URL environment variable to DATABASE_URL in database config

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -98,6 +98,8 @@ return [
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
+
+        
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
